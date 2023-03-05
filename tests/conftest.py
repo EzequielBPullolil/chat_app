f1655430a30eca7118851eb04b0b6d49999d4356
EzequielK-source <programacionezequielk@gmail.com>
2023-03-05 18:29:49 -0300

    Add app and client fixture - Modify sio fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,25 @@
-import socketio
 import pytest
+from src.app import create_app, socketio
 
 @pytest.fixture()
-def sio():
-    sio = socketio.AsyncClient(logger=True, engineio_logger=True)
+def app():
+    app = create_app()
+    app.config.update({
+        'TESTING': True
+    })
+
+    yield app
+
+@pytest.fixture()
+def client(app):
+    return app.test_client()
+
+
+@pytest.fixture()
+def sio(app):
+    flask_client = app.test_client()
+    sio = socketio.test_client(
+        app=app,
+        flask_test_client=flask_client
+    )
     return sio
