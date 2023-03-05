ccc5f72a4e5540721f759ecec2fe3cf61ca769c0
EzequielK-source <programacionezequielk@gmail.com>
2023-03-05 20:13:26 -0300

    Add on_connect event to UserNamespace - if not pass auth param refuse connection

--- src/modules/user/event.py
+++ src/modules/user/event.py
@@ -1,6 +1,9 @@
-from flask_socketio import Namespace
+from flask_socketio import Namespace, ConnectionRefusedError
 from flask_socketio import emit
 class UserNamespace(Namespace):
+    def on_connect(self, auth):
+        if(auth == None): raise ConnectionRefusedError()
+
     def on_send_message(self, message):
         '''
             Emits new_message event to 
