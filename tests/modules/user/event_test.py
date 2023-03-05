e69c7d700143bc45099599f36a8f5c5552b93847
EzequielK-source <programacionezequielk@gmail.com>
2023-03-05 19:56:28 -0300

    Add test case for users namespace connection - Add test for verify if not pass auth params to connect /users namespace refuse connection - Rename file events_test yo event_test

--- tests/modules/user/event_test.py
+++ tests/modules/user/event_test.py
@@ -1,4 +1,18 @@
+from flask_socketio import ConnectionRefusedError
 class TestUserEvents:
+    def test_cant_connect_without_auth_params(self, sio):
+        '''
+            Verify if try connect to namespace /users
+            without auth param refuse connection 
+        '''
+        sio.connect(namespace='/users')
+        assert sio.is_connected() == False
+
+        sio.connect(namespace='/users', auth={
+            'name': 'test'
+        })
+        assert sio.is_connected() == True
+
     def test_send_message_emits_new_message(self, sios):
         '''
             Verify if user emits event 'send_message' to namespace /users 
@@ -9,12 +23,10 @@ class TestUserEvents:
         client2 = sios[1]
         client1.connect(namespace='/users')
         client2.connect(namespace='/users')
-        assert client1.is_connected(namespace='/users')
-        assert client2.is_connected(namespace='/users')
+        assert client1.is_connected(namespace='/users') == True
+        assert client2.is_connected(namespace='/users') == True
 
         client1.emit('send_message', expected_message, namespace='/users')
-
-
         received = client1.get_received(namespace='/users')
         assert received[0]['name'] == 'new_message'
         assert received[0]['args'][0] == expected_message
