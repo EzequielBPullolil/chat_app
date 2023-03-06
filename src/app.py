4657766fb69b53ad062764d057fee78200787cf8
EzequielK-source <programacionezequielk@gmail.com>
2023-03-05 22:35:46 -0300

    extract create_app and add CORS config

--- src/app.py
+++ src/app.py
@@ -1,17 +1,17 @@
 from flask_socketio import SocketIO
+from flask_cors import CORS
 from flask import Flask
 from os import environ
 from .modules.user.event import UserNamespace
 debbug = environ['PYTHONENV'] == 'test' or environ['PYTHONENV'] == 'dev'
 socketio = SocketIO(logger=debbug, engineio_logger=debbug)
-def create_app():
-    '''
-        Creates
-    '''
-    app = Flask(__name__)
 
+app = Flask(__name__)
+socketio.init_app(app,  cors_allowed_origins="*")
+CORS(app)
 
-    socketio.init_app(app)
+    #namespaces implement
+socketio.on_namespace(UserNamespace('/users'))
 
-    socketio.on_namespace(UserNamespace('/users'))
-    return app
+if __name__ == '__main__':
+    socketio.run(app)
