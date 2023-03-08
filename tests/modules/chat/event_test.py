ab896b4d3c446dda4c654d589c41b8e8f2ac0e87
EzequielK-source <programacionezequielk@gmail.com>
2023-03-08 17:29:59 -0300

    create a test case for when a user is not part of a chat and tries to enter - replace all chat_suject_id for chatid

--- tests/modules/chat/event_test.py
+++ tests/modules/chat/event_test.py
@@ -1,10 +1,29 @@
+from pytest import raises
+from src.modules.chat.exceptions.unauthorized_user import UnauthorizedUser
+
+
 class TestChatEvents:
-    def test_send_messsage_to_chat(self, sios, chat_suject_id):
+    def test_trying_to_join_chat_we_are_not_part_of_raise_exception(self, sio, chat_suject):
+        '''
+            Check if user aren part of chat the event 'join_chat' raise exception
+        '''
+        client1 = sio
+        client1.connect(namespace='/chats')
+        assert client1.is_connected() == True
+        with raises(UnauthorizedUser) as exception:
+            client1.emit('join_chat', {
+                'user_id': 'asdiasdiasdniasda',
+                'chat_id': str(chat_suject.inserted_id)
+            }, namespace='/chats')
+            assert exception.type is UnauthorizedUser
+
+    def test_send_messsage_to_chat(self, sios, chat_suject):
         '''
             Emit send_message event in chat namespace
             catch chats id and emit new_message to
             all users in the same chat
         '''
+        chatid = str(chat_suject.inserted_id)
         client1 = sios[0]
         client2 = sios[1]
 
@@ -15,13 +34,13 @@ class TestChatEvents:
         assert client2.is_connected() == True
 
         # users join chat
-        client1.emit('join_chat', chat_suject_id, namespace='/chats')
-        client2.emit('join_chat', chat_suject_id, namespace='/chats')
+        client1.emit('join_chat', chatid, namespace='/chats')
+        client2.emit('join_chat', chatid, namespace='/chats')
 
         client1.emit('send_message', {
             'message': 'hi',
             'username': 'client1',
-            'chatid': chat_suject_id
+            'chatid': chatid
         }, namespace='/chats')
 
         received = client2.get_received(namespace='/chats')
@@ -30,11 +49,12 @@ class TestChatEvents:
         assert received[0]['name'] == 'new_message'
         assert received[0]['args'][0]['username'] == 'client1'
 
-    def test_users_who_arent_part_of_chat_no_receive_new_message_event(self, sios, chat_suject_id):
+    def test_users_who_arent_part_of_chat_no_receive_new_message_event(self, sios, chat_suject):
         '''
             Check if only the users in the same chat receive the event
             'new_message'
         '''
+        chatid = str(chat_suject.inserted_id)
         client1 = sios[0]
         client2 = sios[1]
         client_out_of_chat = sios[2]
@@ -49,13 +69,13 @@ class TestChatEvents:
         assert client2.is_connected() == True
         assert client_out_of_chat.is_connected() == True
         # client1 and client2 join chat
-        client1.emit('join_chat', chat_suject_id, namespace='/chats')
-        client2.emit('join_chat', chat_suject_id, namespace='/chats')
+        client1.emit('join_chat', chatid, namespace='/chats')
+        client2.emit('join_chat', chatid, namespace='/chats')
         # client emit message
         client1.emit('send_message', {
             'message': 'hi',
             'username': 'client1',
-            'chatid': chat_suject_id
+            'chatid': chatid
         }, namespace='/chats')
 
         received = client_out_of_chat.get_received(namespace='/chats')
