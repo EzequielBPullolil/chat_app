823ccc622cffd5862b4ebfc796f761a59e9d8d91
EzequielK-source <programacionezequielk@gmail.com>
2023-03-08 18:42:45 -0300

    Add validations and specifications in documentation - Now user_belongs_to_chat functio not admit ObjectID instance params

--- src/modules/chat/helper/user_belongs_to_chat.py
+++ src/modules/chat/helper/user_belongs_to_chat.py
@@ -7,13 +7,23 @@ from ..exceptions.unauthorized_user import UnauthorizedUser
 def user_belongs_to_chat(chat_id, user_id):
     '''
         Return true if user are member of chat with chat_id
+
+        chat_id and user_id has to be a no ObjectId instance
     '''
-    result = chat.find_one(filter={'_id': ObjectId(chat_id)})
+    if (type(chat_id) == ObjectId):
+        raise Exception('chat_id must be non ObjectID param')
+
+    if (type(user_id) == ObjectId):
+        raise Exception('user_id must be non ObjectID param')
+
+    chatid = ObjectId(chat_id)
+    userid = ObjectId(user_id)
+    result = chat.find_one(filter={'_id': chatid})
 
     members = result['members']
 
     for user in members:
-        if (ObjectId(user_id) == user['_id']):
+        if (userid == user['_id']):
             return True
 
     raise UnauthorizedUser(chat_id=chat_id, user_id=user_id)
