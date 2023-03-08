80f0fcf23d5889f7695ae7e7202d614455ee7113
EzequielK-source <programacionezequielk@gmail.com>
2023-03-08 17:33:12 -0300

    Now join_chat verify if user_id are part of members in chat

--- src/modules/chat/event.py
+++ src/modules/chat/event.py
@@ -1,9 +1,12 @@
+from bson import ObjectId
 from flask_socketio import Namespace, join_room, emit
 from flask import request
 
+from .exceptions.unauthorized_user import UnauthorizedUser
+from src.databases.mongodb import chat
+
 
 class ChatNamespace(Namespace):
-
     def on_send_message(self, data):
         '''
             Emits to all users in the same chat 
@@ -16,6 +19,17 @@ class ChatNamespace(Namespace):
             'message': data['message']
         }, to=data['chatid'])
 
-    def on_join_chat(self, chatid):
-        join_room(chatid)
-        print(f'user join to chat {chatid}')
+    def on_join_chat(self, data):
+        '''
+            Verify if user with data['user_id'] are part 
+            of members of the chat with id == data['chat_id'] 
+        '''
+        user_id = data['user_id']
+        chat_id = data['chat_id']
+
+        result = chat.find_one(filter={"_id": ObjectId(chat_id)})
+        if (not user_id in result['members']):
+            raise UnauthorizedUser(user_id=user_id, chat_id=chat_id)
+
+        join_room(chat_id)
+        print(f'user: {user_id} join to chat: {chat_id}')
