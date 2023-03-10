69571b0c578ceb0748dd7ce8558dc8a07e0e355a
EzequielK-source <programacionezequielk@gmail.com>
2023-03-10 17:25:58 -0300

    Add testcase for raises UnauthorizedUser when user is unrelated

--- tests/modules/chat/helper/user_belongs_to_chat_test.py
+++ tests/modules/chat/helper/user_belongs_to_chat_test.py
@@ -1,10 +1,11 @@
+from bson import ObjectId
 from src.modules.chat.helper.user_belongs_to_chat import user_belongs_to_chat
 import pytest
 from src.databases.mongodb import chat
+from src.modules.chat.exceptions.unauthorized_user import UnauthorizedUser
 
 
 class TestUsersBelongsToChatHelper:
-
     def test_raise_valueerror_if_chat_not_are_collection_instance(self):
         '''
             Throw error if chat param is not a dict
@@ -20,3 +21,19 @@ class TestUsersBelongsToChatHelper:
             user_belongs_to_chat(chat={}, user='a')
 
             assert e_info.message == "the 'user' parameter is not a dict"
+
+    def test_passing_an_unrelated_user_and_chat_raise_UnauthorizedUser(self, chat_suject):
+        '''
+            check if parse an user who arent part of chat members
+            as params of user_belongs_to_chat raise UnauthorizedUser
+            exception
+        '''
+        user = {
+            '_id': ObjectId()
+        }
+        chat_param = chat.find_one(filter={'_id': chat_suject.inserted_id})
+
+        with pytest.raises(UnauthorizedUser) as e_info:
+            user_belongs_to_chat(user=user, chat=chat_param)
+
+            assert e_info.message == f"the user with id {user['_id']} arent part of chat with id {chat_param['_id']}"
