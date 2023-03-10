7de8ba65eb5c443f906f77383ed0e6346a02e865
EzequielK-source <programacionezequielk@gmail.com>
2023-03-10 17:44:42 -0300

    Modify test script - clear console before run pytest - prints MONGODB and PYTHON env variables

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,4 +4,9 @@ export PYTHONENV='test'
 
 export MONGODB_URI='mongodb://localhost:27017'
 export MONGODB_NAME='chat_app'
+
+clear
+echo "test start with PYTHONPATH=${PYTHONPATH} and PYTHONENV=${PYTHONENV}"
+echo "And MONGODB_URI=${MONGODB_URI} and MONGODB_NAME=${MONGODB_NAME}"
+sleep 2
 pytest -s -vvvv $1 $2
