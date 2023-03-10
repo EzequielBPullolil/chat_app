a6504b47acf62e386d74e05aec8feb3668154be5
EzequielK-source <programacionezequielk@gmail.com>
2023-03-10 17:13:29 -0300

    Add testcase for not dict user parameter

--- tests/modules/chat/helper/user_belongs_to_chat_test.py
+++ tests/modules/chat/helper/user_belongs_to_chat_test.py
@@ -5,7 +5,7 @@ from src.databases.mongodb import chat
 
 class TestUsersBelongsToChatHelper:
 
-    def test_raise_valueerror_if_chat_not_are_collection_instance(self, chat_suject):
+    def test_raise_valueerror_if_chat_not_are_collection_instance(self):
         '''
             Throw error if chat param is not a dict
         '''
@@ -14,3 +14,9 @@ class TestUsersBelongsToChatHelper:
             user_belongs_to_chat(user={}, chat='a')
 
             assert e_info.message == "the 'chat' parameter is not a dict"
+
+    def test_raise_valueerror_if_user_not_are_collection_instace(self):
+        with pytest.raises(ValueError) as e_info:
+            user_belongs_to_chat(chat={}, user='a')
+
+            assert e_info.message == "the 'user' parameter is not a dict"
