ea463b3caa4ed90f54bdddbad3fd2b7463aba8c7
EzequielK-source <programacionezequielk@gmail.com>
2023-03-13 15:48:14 -0300

    Fixes and imports - Import ObjectID and UnauthorizedUser exception - change 'error' to 'exception' from the test names

--- tests/modules/chat/event/send_message_test.py
+++ tests/modules/chat/event/send_message_test.py
@@ -1,12 +1,17 @@
 import pytest
+from bson import ObjectId
+
+from src.modules.chat.exceptions.unauthorized_user import UnauthorizedUser
 
 
 class TestSendMessageEvent:
-    def test_missing_user_id_raise_error(self, sio, chat_memebers):
+    def test_missing_user_id_raise_exception(self, sio):
         '''
             Check if missing user_id data param throw error
         '''
         sio.connect(namespace='/chats')
+        assert sio.is_connected(namespace='/chats') == True
+
         with pytest.raises(ValueError) as e_info:
             sio.emit('send_message', {
                 'chat_id': 'asdasdasdasd',
@@ -14,7 +19,7 @@ class TestSendMessageEvent:
             }, namespace='/chats')
             assert 'missing user_id data param' in str(e_info.value)
 
-    def test_missing_chat_id_raise_error(self, sio):
+    def test_missing_chat_id_raise_exception(self, sio):
 
         sio.connect(namespace='/chats')
         assert sio.is_connected(namespace='/chats') == True
