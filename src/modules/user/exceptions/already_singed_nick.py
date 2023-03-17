31f7e4721e286d2e72b36067fc0f961c9664dd13
EzequielK-source <programacionezequielk@gmail.com>
2023-03-17 16:54:30 -0300

    Add '@' to message exception

--- src/modules/user/exceptions/already_singed_nick.py
+++ src/modules/user/exceptions/already_singed_nick.py
@@ -4,4 +4,4 @@ from . import UserDomainException
 class AlreadySingedNick(UserDomainException):
     def __init__(self, *args: object, nick):
         super().__init__(*args)
-        self.message = str(f'the nick {nick} is already singed')
+        self.message = str(f'the nick @{nick} is already singed')
