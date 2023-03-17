66f2a2fde5a95829f8c9f37d066ba65a68dabddb
EzequielK-source <programacionezequielk@gmail.com>
2023-03-17 15:26:46 -0300

    Add response in case of missing payloads name and nick - Rename function for create_user to sing_user - simplify KeyError except block - add documentation to route

--- src/modules/user/routes.py
+++ src/modules/user/routes.py
@@ -4,17 +4,34 @@ user_bp = Blueprint('user', __name__)
 
 
 @user_bp.route('/', methods=['POST'])
-def create_user():
+def sing_user():
     '''
-        Persist user in database
+        Persist user before validate name, password and nick,
+        and response a json with name, nickname and encripted password
+
+        required payloads: name, nick, password
+
+        response: JSON, status 201
     '''
     try:
         data = request.get_json()
         password = data['password']
+        nick = data['nick']
+        name = data['name']
+
         return {}, 201
 
     except KeyError:
+        missings_param = ''
         if (not 'password' in data):
-            return {
-                'error': 'missing password payload data'
-            }, 400
+            missings_param = 'password'
+
+        if (not 'nick' in data):
+            missings_param = 'nick'
+
+        if (not 'name' in data):
+            missings_param = 'name'
+
+        return {
+            'error': f'missing {missings_param} payload data'
+        }, 400
