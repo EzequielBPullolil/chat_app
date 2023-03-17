c7501948409d3dcf8f0c48f9be3307cd733edb55
EzequielK-source <programacionezequielk@gmail.com>
2023-03-17 16:30:22 -0300

    Create AlreadySingedNick exception

---
+++ src/modules/user/exceptions/already_singed_nick.py
@@ -0,0 +1,7 @@
+from . import UserDomainException
+
+
+class AlreadySingedNick(UserDomainException):
+    def __init__(self, *args: object, nick):
+        super().__init__(*args)
+        self.message = str(f'the nick {nick} is already singed')
