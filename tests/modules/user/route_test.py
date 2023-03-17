06eb290298ec744a4631fcd1ff6132aabde9372d
EzequielK-source <programacionezequielk@gmail.com>
2023-03-17 15:07:11 -0300

    Add testcase for check response when missing password

--- tests/modules/user/route_test.py
+++ tests/modules/user/route_test.py
@@ -12,3 +12,12 @@ class TestPostUserRoutes:
         response = client.post('/users/', json=post_data)
 
         assert response.status_code == 201
+
+    def test_missing_password_data_response_status_400(self, client):
+        post_data = {
+            'name': 'test name',
+            'nick': '@TestNick'
+        }
+
+        response = client.post('/users/', json=post_data)
+        assert response.status_code == 400
