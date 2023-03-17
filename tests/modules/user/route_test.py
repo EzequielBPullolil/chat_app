c19615dbeb4954a5db0fca7fc12ed4f3c425223c
EzequielK-source <programacionezequielk@gmail.com>
2023-03-17 15:56:00 -0300

    Change name test_post_method for test_valid_case and add assertions - Validate if user key exist in response - Add assertions for name and nick - assert if _id response are valid ObjectId

--- tests/modules/user/route_test.py
+++ tests/modules/user/route_test.py
@@ -1,9 +1,9 @@
-from flask import url_for
+from bson import ObjectId
 
 
 class TestPostUserRoutes:
     # Root route
-    def test_post_method(self, client):
+    def test_valid_case(self, client):
         post_data = {
             'name': 'test name',
             'nick': '@TestNick',
@@ -12,6 +12,12 @@ class TestPostUserRoutes:
         response = client.post('/users/', json=post_data)
 
         assert response.status_code == 201
+
+        responsed_user = response.json['user']
+
+        assert ObjectId.is_valid(responsed_user['_id']) == True
+        assert responsed_user['name'] == post_data['name']
+        assert responsed_user['nick'] == post_data['nick']
 
     def test_missing_password_data_response_status_400(self, client):
         post_data = {
