0a860e5e9a829cca40fc09ea6118a7b040b0257e
EzequielK-source <programacionezequielk@gmail.com>
2023-03-17 15:12:38 -0300

    Add validation for missing payload password

--- src/modules/user/routes.py
+++ src/modules/user/routes.py
@@ -5,4 +5,16 @@ user_bp = Blueprint('user', __name__)
 
 @user_bp.route('/', methods=['POST'])
 def create_user():
-    return {}, 201
+    '''
+        Persist user in database
+    '''
+    try:
+        data = request.get_json()
+        password = data['password']
+        return {}, 201
+
+    except KeyError:
+        if (not 'password' in data):
+            return {
+                'error': 'missing password payload data'
+            }, 400
