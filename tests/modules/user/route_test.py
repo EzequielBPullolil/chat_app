0ad002db36aef97a269ee3a5bf6de817d0973000
EzequielK-source <programacionezequielk@gmail.com>
2023-03-17 14:48:12 -0300

    Create test for user routes - Add testcase for POST method

---
+++ tests/modules/user/route_test.py
@@ -0,0 +1,14 @@
+from flask import url_for
+
+
+class TestUserRoutes:
+    # Root route
+    def test_post_method(self, client):
+        post_data = {
+            'name': 'test name',
+            'nick': '@TestNick',
+            'password': 'aPassword'
+        }
+        response = client.post('/users/', json=post_data)
+
+        assert response.status_code == 201
